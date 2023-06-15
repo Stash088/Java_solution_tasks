68d652f89fa98b7947be8aeb38fcff523fdeefb7
Amir <79719443+Stash088@users.noreply.github.com>
2023-06-15 13:16:27 +0300

    Create getMiddle.java

---
+++ getMiddle.java
@@ -0,0 +1,10 @@
+class Kata {
+  public static String getMiddle(String s) {
+    if(s.length() % 2 == 0){
+            return(s.substring(s.length()/ 2 - 1,(s.length() /2  + 1)));
+        }
+        else{
+            return String.valueOf(s.charAt(s.length() / 2));
+        }
+  }
+}
