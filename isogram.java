ff807c35dddbd377536241a026e146cd58720a35
Amir <79719443+Stash088@users.noreply.github.com>
2023-06-15 11:17:43 +0300

    Update isogram.java

--- isogram.java
+++ isogram.java
@@ -1,18 +1,17 @@
 import java.util.*;
-import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class isogram {
-    public static boolean  isIsogram(String str) {
-        List<String> items = Arrays.asList(str.toLowerCase().split("(?!^)"));
-        HashSet<String> countryHashSet = new HashSet<>();
-        for(String i : items){
-            countryHashSet.add(i);
-        }
-        if(items.size() == countryHashSet.size()){
-            return true;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        Set<String> set = Stream.of(str.trim().split("(?!^)")) .collect(Collectors.toSet());
+        if(str.length() == set.size()){
+            System.out.println("Yes");
         }
         else{
-            return false;
+            System.out.println("No");
         }
     }
-} 
+}
